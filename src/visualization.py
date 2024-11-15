b49da7e2c761105d0aba24a39cbfe1ba992dd8fe
Paul O'Reilly <oreilp11@tdc.ie>
2024-11-15 12:36:13 +0000

    added os paths for os cross compatibility (to work on my device)

--- src/visualization.py
+++ src/visualization.py
@@ -2,12 +2,18 @@ from flask import Flask, render_template, jsonify
 import update_satellite_positions
 from find_shortest_way import find_shortest_path
 import csv
+import os
 from wind_farm import WindTurbineNode
 
-app = Flask(__name__, template_folder='../templates', static_folder='../static')
+base_path = os.path.dirname(os.path.dirname(__file__))
+static_path = os.path.join(base_path, 'static')
+template_path = os.path.join(base_path, 'templates')
+devices_path = os.path.join(base_path, 'assets', 'devices_ip.csv')
+
+app = Flask(__name__, template_folder=template_path, static_folder=static_path)
 
 # Initialize WindTurbineNode to access generate_turbine_data
-turbine_node = WindTurbineNode('../assets/devices_ip.csv', update_satellite_positions.calculate_satellite_positions())
+turbine_node = WindTurbineNode(devices_path, update_satellite_positions.calculate_satellite_positions())
 
 @app.route('/')
 def index():
@@ -16,7 +22,7 @@ def index():
 @app.route('/get_positions')
 def get_positions():
     positions = update_satellite_positions.calculate_satellite_positions()
-    with open('../assets/devices_ip.csv', mode='r') as file:
+    with open(devices_path, mode='r') as file:
         # add name for each position
         reader = csv.DictReader(file)
         for row in reader:
